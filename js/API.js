d9ac84a477ed9b3c6078db631ff4c8043e4ad57e
Rodrigo <pieroni.rodrigo@gmail.com>
2023-09-26 15:12:49 -0300

    Subida1

---
+++ js/API.js
@@ -0,0 +1,67 @@
+const url='http://localhost:4000/clientes';
+///cuando se crea un nuevo cliente
+export const nuevoCliente = async cliente =>{
+
+try {
+    await fetch(url, {
+     method: 'POST',
+     body: JSON.stringify( cliente ),
+     headers: {
+        'Content-Type':'application/json'
+        }
+    });
+    window.location.href='index.html';
+} catch (error) {
+    console.log(error);
+}
+    console.log('conectado...');
+}
+
+//Obtiene todos los clientes
+export const obtenerCliente = async () =>{
+    try {
+        const resultado = await fetch(url);
+        const clientes = await resultado.json();
+        return clientes;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const eliminarCliente = async id =>{
+    try {
+        await fetch(`${url}/${id}`,{
+        method:'DELETE'
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const obtenerClienteEdit = async id =>{
+    try {
+        const resultado = await fetch(`${url}/${id}`);    
+        const cliente = await resultado .json();
+        return  cliente ;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+//Actualizar Registro
+export const editarCliente = async cliente =>{
+    try {
+        console.log("uuu: ", cliente.id);
+
+        await fetch(`${url}/${cliente.id}`,{
+            method:'PUT',
+            body:JSON.stringify(cliente),
+            headers:{
+                'Content-Type':'application/json'
+            }
+        });
+        window.location.href ='index.html';     
+    } catch (error) {
+        console.log(error);
+    }
+}
